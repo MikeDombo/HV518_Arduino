b94ace514b07bd5b494d1aaa6d539f5129258544
Michael Dombrowski <mikepi3.14@gmail.com>
2017-12-20 23:07:19 -0500

    Add comments and Serial help

--- example/VFD.ino
+++ example/VFD.ino
@@ -1,10 +1,7 @@
 #include "HV518.h"
 
-const uint8_t brightnessMax = 255;
-const uint8_t brightnessMin = 33;
-int msInSecond = 1000;
-
 // DIN, CLK, LATCH, Number of Digits, Total Shift Lines, left align display
+// No dimming using the strobe line. This example shows dimming using anode PWM
 HV518 disp = HV518(5, 6, 7, 6, 64, true);
 
 void setup() {
@@ -17,50 +14,75 @@ uint8_t hours = 0;
 uint8_t minutes = 0;
 uint8_t seconds = 0;
 boolean timeSet = false;
+int msInSecond = 1000;
 uint8_t brightness = 255;
 
 void loop() {
-  // Input time as HH,MM,SS
   while(Serial.available() > 0){
     char first = Serial.read();
+
+    // Set time as tHH,MM,SS
     if(first == 't'){
       hours = Serial.parseInt();
       minutes = Serial.parseInt();
       seconds = Serial.parseInt();
+      Serial.println("Time set to " + String(hours) + ":" + String(minutes) + ":" + String(seconds));
       if (Serial.read() == '\n') {
         timeSet = true;
       }
     }
+    
+    // Brighness
     else if(first == 'b'){
       brightness = Serial.parseInt();
+      Serial.println("Brightness set to " + String(brightness) + " / 255");
+      if (Serial.read() == '\n') {
+        return;
+      }
     }
+    
+    // Adjust tick time (milliseconds per second)
     else if(first == 'a'){
       msInSecond += Serial.parseInt();
-      Serial.println(msInSecond);
+      Serial.println("Tick adjusted to " + String(msInSecond) + " ms/s");
+      if (Serial.read() == '\n') {
+        return;
+      }
     }
+    
+    // Print a string
     else if(first == 's'){
       timeSet = false;
       String str = Serial.readString();
-      Serial.print(str);
+      Serial.print("Displaying input string: " + str);
       disp.writeString(0, str);
       disp.displayWithAnodePWM(brightness, 1000);
     }
   }
 
+  // Display the time
   if(timeSet){
+    // Clear the display memory before writing into it
     disp.clearDisplayMemory();
+
+    // Write time into display memory
     disp.writeNumber(0, hours, 2);
     disp.writeNumber(2, minutes, 2);
     disp.writeNumber(4, seconds, 2);
+    
+    // Alternate between colon line and dot
     if(seconds % 2 == 0){
       disp.setDigit(6, disp.specialChars.colonDot);
     }
     else{
       disp.setDigit(6, disp.specialChars.colonLine);
     }
+
+    // Write to display
     disp.displayWithAnodePWM(brightness, msInSecond);
 
-    seconds++;
+    seconds++; // Tick
+    
     // Carry-over
     if(seconds == 60){
       seconds = 0;
@@ -75,4 +97,3 @@ void loop() {
     }
   }
 }
-
